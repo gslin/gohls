7227f409abab8b9db6442d60fda58f2c2d977b17
Gea-Suan Lin <gslin@gslin.org>
2015-03-29 11:19:13 +0800

    Put distribution into version.

--- ppa.sh
+++ ppa.sh
@@ -3,6 +3,6 @@
 DISTS="precise trusty"
 
 for DIST in ${DISTS}; do
-    sed "s/%%DIST%%/${DIST}/" debian/changelog.template > debian/changelog
+    sed "s/%%DIST%%/${DIST}/g" debian/changelog.template > debian/changelog
     debuild --no-tgz-check -S
 done
