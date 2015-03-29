21d4e63a8726c0e41a433ecccc6be8d3f8d1e9b9
Gea-Suan Lin <gslin@gslin.org>
2015-03-29 11:21:40 +0800

    Run dput to upload to PPA.

--- ppa.sh
+++ ppa.sh
@@ -5,4 +5,5 @@ DISTS="precise trusty"
 for DIST in ${DISTS}; do
     sed "s/%%DIST%%/${DIST}/g" debian/changelog.template > debian/changelog
     debuild --no-tgz-check -S
+    (cd ..; dput ppa:gslin/gohls "gohls_1.0.5-1ppa1~${DIST}_source.changes")
 done
